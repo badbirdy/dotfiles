d85dd979e5f3f791f1100164145b5b4dad8ad962
bdbd2yy <ysihigh74@gmail.com>
2026-07-13 01:52:11 +0800

    show hidden files in nvim

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,19 @@ return {
 
   opts = {
 
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+
     dashboard = {
 
       preset = {
